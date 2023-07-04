bb35299dc7cdfe51d901b247acce481c329601dd
cristiangomezdev <cristiangomez.dev@gmail.com>
2023-07-04 18:41:00 -0300

    feat cloudinary

--- backend/app/Http/Controllers/Api/TemplateController.php
+++ backend/app/Http/Controllers/Api/TemplateController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
+
 use App\Http\Controllers\Controller;
 use App\Models\Template;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Cloudinary;
 
 class TemplateController extends Controller
 {
@@ -23,8 +25,11 @@ class TemplateController extends Controller
     }
     public function store(Request $request)
     {
+        $uploadedFile = $request->file('image');
+        
+        $image = Cloudinary::upload($uploadedFile->getRealPath());
         $template = new Template;
-        $template->urlImg = $request->urlImg;
+        $template->urlImg = $image->getSecurePath();
         $template->name = $request->name;
         $template->save();
 
