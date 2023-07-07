485c0d49e43197393607bebd7a58c12f0e79f023
JoaquinJS18 <jhinfire@gmail.com>
2023-07-07 15:56:25 -0300

    Validacion a estudiantes agregada.

--- backend/app/Http/Controllers/Api/StudentController.php
+++ backend/app/Http/Controllers/Api/StudentController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Student;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 use Symfony\Component\HttpFoundation\Response;
 
@@ -34,6 +36,18 @@ class StudentController extends Controller
 
     public function store(Request $request){
         try {
+            // Validar los datos de entrada
+            $validator = Validator::make($request->all(), [
+                'DNI' => 'required|numeric|unique:students,DNI',
+                'name' => 'required',
+                'lastname' => 'required',
+                'email' => 'required|email|unique:students,email',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->errors()], Response::HTTP_BAD_REQUEST);
+            }
+
             $student = new Student();
 
             $student->DNI = $request->DNI;
@@ -52,6 +66,18 @@ class StudentController extends Controller
     public function update(Request $request, $id){
         try {
             $student = Student::findOrFail($id);
+
+            // Validar los datos de entrada
+            $validator = Validator::make($request->all(), [
+                'DNI' => ['required','numeric',Rule::unique('students')->ignore($id,'_id')],
+                'name' => 'required',
+                'lastname' => 'required',
+                'email' => ['required','email',Rule::unique('students')->ignore($id,'_id')],
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->errors()], Response::HTTP_BAD_REQUEST);
+            }
 
             $student->DNI = $request->DNI;
             $student->name = $request->name;
