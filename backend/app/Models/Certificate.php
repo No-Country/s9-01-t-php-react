480c78007e1e59b3139092603bd74270e18ac2cc
cristiangomezdev <cristiangomez.dev@gmail.com>
2023-07-12 15:21:50 -0300

    fixing relations

--- backend/app/Models/Certificate.php
+++ backend/app/Models/Certificate.php
@@ -27,12 +27,47 @@ class Certificate extends Model
         'id_cd'  => 'string',
         'public_key' => 'string'
     ];
+
     public function student()
     {
         return $this->belongsTo(Student::class, 'id_student');
     }
+
     public function certificateData()
     {
         return $this->belongsTo(CertificateData::class, 'id_cd');
     }
+    public function logo()
+    {
+        return $this->belongsTo(Logo::class, 'id_logo');
+    }
+    public function template()
+    {
+        return $this->belongsTo(Template::class, 'id_template');
+    }
+    protected $hidden = [
+        'student',
+        'certificateData',
+        'logo',
+        'template'
+    ];
+     public function toArray()
+    {
+        $array = parent::toArray();
+
+        if ($this->relationLoaded('student')) {
+            $array['id_student'] = $this->student;
+        }
+
+        if ($this->relationLoaded('certificateData')) {
+            $array['id_cd'] = $this->certificateData;
+        }
+        if ($this->relationLoaded('logo')) {
+            $array['id_logo'] = $this->logo;
+        }
+        if ($this->relationLoaded('template')) {
+            $array['id_template'] = $this->template;
+        }
+        return $array;
+    } 
 }
