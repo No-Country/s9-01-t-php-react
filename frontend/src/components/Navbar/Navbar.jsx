8c0f63057e6c0b0b599bc70250650504e530e8f0
fabianeromano <fabian.ed.romano@gmail.com>
2023-07-18 00:50:32 -0300

    feat: faq & fix nav

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,43 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { CertificateIcon, HomeIcon, ProfileIcon, StarIcon } from "../Icons/Icons";
 
 const Navbar = () => {
+  const { isAuth } = useSelector(state => state.auth);
   return (
-    <nav className="max-w-[360px] w-full h-10 flex justify-between items-center px-10">
+    <nav className="h-[80px] w-full bg-[#D9D9D9] flex justify-between items-end pb-3 pr-[80px] pl-[42px]">
       <Link to="/">
-        <HomeIcon />
+        <div className="w-[143px] h-10 bg-[#E6E6E6]"></div>
       </Link>
-      <Link to="/dashboard">
-        <CertificateIcon />
-      </Link>
-      <Link>
-        <StarIcon />
-      </Link>
-      <Link to="/profile">
-        <ProfileIcon />
-      </Link>
+      <ul className="flex gap-10 pb-2">
+        <li className="font-normal text-sm left-6">Sobre nosotros</li>
+        <li className="font-normal text-sm left-6">Planes</li>
+        <li className="font-normal text-sm left-6">Plantillas</li>
+        <Link to="/faq">
+          <li className="font-normal text-sm left-6">Preguntas Frecuentes</li>
+        </Link>
+      </ul>
+      <div className="flex gap-2 text-white">
+        {isAuth ? (
+          <Link to="/home">
+            <button className="w-[140px] h-8 border border-[#959595] rounded-[10px] font-normal text-sm leading-6">
+              Ingresar
+            </button>
+          </Link>
+        ) : (
+          <>
+            <Link to="/login">
+              <button className="w-[140px] h-8 border border-[#959595] rounded-[10px] font-normal text-sm leading-6">
+                Login
+              </button>
+            </Link>
+            <Link to="/register">
+              <button className="w-[140px] h-8 bg-[#7F807F] border border-[#959595] rounded-[10px] font-normal text-sm leading-6">
+                Register
+              </button>
+            </Link>
+          </>
+        )}
+      </div>
     </nav>
   );
 };
