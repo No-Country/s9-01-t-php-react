b674a981ff18ec9d4ce57f7fd8dde49100869650
fabianeromano <fabian.ed.romano@gmail.com>
2023-07-18 23:20:54 -0300

    feat: account pages

---
+++ frontend/src/pages/account.jsx
@@ -0,0 +1,42 @@
+import Layout from "../components/Layout/Layout";
+import LayoutAccount from "../components/Layout/LayoutAccount";
+
+const Account = () => {
+  return (
+    <Layout>
+      <LayoutAccount>
+        <div className="flex flex-col w-full h-full">
+          <h1 className="font-semibold text-2xl leading-6 pl-[56px]">Mi cuenta</h1>
+          <div className="flex items-center justify-between px-[85px] mt-[45px]">
+            <p className="font-normal text-sm left-6">Editar mail</p>
+            <input
+              type="text"
+              placeholder="Placeholder Text"
+              className="w-[486px] h-[48px] rounded-[10px] border-[3px] border-[#E4E4E4] pl-4"
+            />
+          </div>
+          <div className="flex items-center justify-between px-[85px] mt-[24px]">
+            <p className="font-normal text-sm left-6">Cambiar contraseña</p>
+            <input
+              type="text"
+              placeholder="Placeholder Text"
+              className="w-[486px] h-[48px] rounded-[10px] border-[3px] border-[#E4E4E4] pl-4"
+            />
+          </div>
+          <div className="w-full flex justify-end pr-[85px]">
+            <button className="w-[328px] h-[48px] bg-[#7F807F] rounded-[10px] font-bold text-[17px] leading-6 text-white mt-[60px]">
+              Guardar cambios
+            </button>
+          </div>
+          <div className="flex-1 flex justify-end items-end pr-[85px]">
+            <button className="w-[328px] h-[48px] bg-[#7F807F] rounded-[10px] font-bold text-[17px] leading-6 text-white mt-[60px]">
+              Eliminar cuenta
+            </button>
+          </div>
+        </div>
+      </LayoutAccount>
+    </Layout>
+  );
+};
+
+export default Account;
--- frontend/src/routes/App.jsx
+++ frontend/src/routes/App.jsx
@@ -5,11 +5,14 @@ import Register from "../pages/register";
 import ErrorPage from "../pages/error-page";
 import { UserRouteProtected } from "./user/userRouteProtected";
 import Dashboard from "../pages/dashboard";
-import Profile from "../pages/profile";
 import Landing from "../pages/landing";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Faq from "../pages/faq";
+import Account from "../pages/account";
+import CurrentPlan from "../pages/currentPlan";
+import Support from "../pages/support";
+import PaymentHistory from "../pages/paymentHistory";
 
 function App() {
   return (
@@ -23,8 +26,11 @@ function App() {
           <Route path="/faq" element={<Faq />} />
           <Route element={<UserRouteProtected />}>
             <Route path="/home" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/currentplan" element={<CurrentPlan />} />
+            <Route path="/paymenthistory" element={<PaymentHistory />} />
+            <Route path="/support" element={<Support />} />
           </Route>
         </Routes>
       </BrowserRouter>
